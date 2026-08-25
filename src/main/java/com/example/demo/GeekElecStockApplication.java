a512ae9afa1003f48664ab248175863a69719f4e
Sasaki-Momoka <momosasa39@gmail.com>
2026-08-25 13:58:46 +0900

    create main

---
+++ src/main/java/com/example/demo/GeekElecStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekElecStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekElecStockApplication.class, args);
+	}
+
+}
